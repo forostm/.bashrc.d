2ddb41323910ee4075e2dd88429455bcd82e4b21
Iván Barcia <mercauditore@gmail.com>
2015-11-02 19:41:47 +0100

    Eliminado update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-# Esto actualiza tódolos arquivos
-
-# bashrc
-wget -O $HOME/.bashrc https://gist.githubusercontent.com/barcia/46848b3e53435ad3ee68/raw/.bashrc
-
-# bashrc directorio
-git -C $HOME.bashrc.d/ pull
