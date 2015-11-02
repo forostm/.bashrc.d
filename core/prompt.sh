9d53e74b75c8f46e77afc09f1440181a493cbd3f
Iván Barcia <mercauditore@gmail.com>
2015-11-02 21:04:49 +0100

    colores prompt

--- core/prompt.sh
+++ core/prompt.sh
@@ -1,5 +1,15 @@
 # Configuración do prompt
-export PS1='\[\e[1;33m\]\u\[\e[m\]\[\e[1;30m\]@\[\e[m\]\[\e[1;33m\]\h\[\e[m\] \[\e[1;34m\]\w\[\e[m\]\[\e[1;30m\]\$\[\e[m\]\[\e[1;35m\]$(__git_ps1 " (%s)")\[\e[m\] \[\e[1;30m\]>\[\e[m\] '
+## Colores por equipos
+
+if [ $(hostname) == razzmatazz ]; then
+  HOST_COLOR="1;35m"
+elif [ $(hostname) == unicorn ]; then
+  HOST_COLOR="1;33m"
+else
+  HOST_COLOR="1;33m"
+fi
+
+export PS1='\[\e[1;33m\]\u\[\e[m\]\[\e[1;30m\]@\[\e[m\]\[\e['$HOST_COLOR'\]\h\[\e[m\] \[\e[1;34m\]\w\[\e[m\]\[\e[1;30m\]\$\[\e[m\]\[\e[1;35m\]$(__git_ps1 " (%s)")\[\e[m\] \[\e[1;30m\]>\[\e[m\] '
 
 # Benvida
 echo -n "Hola $USER, hoxe é "; date '+%A %-d de %B do %Y'
