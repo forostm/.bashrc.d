590bd868f4ea55d3cf2805a6ec86d13dea60c0d1
Iván Barcia <mercauditore@gmail.com>
2015-11-06 00:31:59 +0100

    Nova funcion: folders

---
+++ functions/folders.sh
@@ -0,0 +1,21 @@
+# Función para crear/entrar nun cartafol rápido para relizar tarefas puntuais
+
+work() {
+    FOLDER_NAME="work"
+    create_folder
+}
+
+temp() {
+  FOLDER_NAME="temp"
+  create_folder
+}
+
+function create_folder() {
+    if [ -d "$HOME/$FOLDER_NAME/" ]; then
+        cd "$HOME/$FOLDER_NAME/"
+        ls
+    else
+        mkdir "$HOME/$FOLDER_NAME/"
+        cd "$HOME/$FOLDER_NAME/"
+    fi
+}
