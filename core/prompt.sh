9e1c39bb573aa0459b87363bb30146d5464a1430
Iván Barcia <mercauditore@gmail.com>
2015-11-10 16:41:39 +0100

    Update prompt.sh

--- core/prompt.sh
+++ core/prompt.sh
@@ -1,6 +1,14 @@
 # Configuración do prompt
-## Colores por equipos
+## Colores por usuarios
+if [ '$USER' == root ]; then
+  USER_COLOR="1;31m"
+elif [ '$USER' == ivan ]; then
+  USER_COLOR="1;33m"
+else
+  USER_COLOR="1;32m"
+fi
 
+## Colores por equipos
 if [ $(hostname) == razzmatazz ]; then
   HOST_COLOR="1;35m"
 elif [ $(hostname) == unicorn ]; then
@@ -11,7 +19,7 @@ else
   HOST_COLOR="1;33m"
 fi
 
-export PS1='\[\e[1;33m\]\u\[\e[m\]\[\e[1;30m\]@\[\e[m\]\[\e['$HOST_COLOR'\]\h\[\e[m\] \[\e[1;34m\]\w\[\e[m\]\[\e[1;30m\]\$\[\e[m\]\[\e[1;35m\]$(__git_ps1 " (%s)")\[\e[m\] \[\e[1;30m\]>\[\e[m\] '
+export PS1='\[\e['$USER_COLOR'\]\u\[\e[m\]\[\e[1;30m\]@\[\e[m\]\[\e['$HOST_COLOR'\]\h\[\e[m\] \[\e[1;34m\]\w\[\e[m\]\[\e[1;30m\]\$\[\e[m\]\[\e[1;35m\]$(__git_ps1 " (%s)")\[\e[m\] \[\e[1;30m\]>\[\e[m\] '
 
 # Benvida
 echo -n "Hola $USER, hoxe é "; date '+%A %-d de %B do %Y'
